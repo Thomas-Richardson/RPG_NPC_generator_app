b2f5e1b4967de3e0d06078b05bb62783b72b5e5d
Thomas-Richardson <thomas.m.richardson@protonmail.com>
2023-01-15 21:22:27 +0000

    split data into txt files

--- app.py
+++ app.py
@@ -4,27 +4,14 @@ import random as rd
 import datetime as dt
 st.set_page_config(layout="wide")
 
-# =========== Material ====================================
-
-personality_traits_list = ['Kind', 'rude/Obnoxious','loud','annoyed','arrogant, braggart', 'overconfident', 'timid', 'postitive', 'Gloomy', 'anxious', 'apologetic', 'pampered','grim', 'people pleaser', 'unreliable', 'wise','friendly', 'stiff, emotionless','ambitious' ,'busy', 'forceful','curious', 'logical','vain', 'hedonistic', 'absent-minded', 'sarcastic', 'intimidating', 'protective over friends', 'quick to anger', 'wise cracking', 'dramatic','slow', 'mysterious', 'reckless', 'liar', 'organised', 'disorganised']
-
-physical_traits_list = ['attractive','tall','fat','short','thin','muscular','ugly','bulbous nose','thin serpentine lips', 'thick lips','long nose','curly hair','bald','long hair', 'styled hair', 'pale skin','swarthy skin','scar','large eyes','high cheekbones','strong jawline', 'old','young', 'tall forehead','crooked teeth', 'obvious cybernetic',]
-
-clothing_list = ['tattoos','make up','cultural dress','rings','piercings','necklace','punk clothing','suit','armour','torn clothing','fashionable clothing', 'colourful clothing', 'moustache','beard', 'Long coat','Hood or veil','Short cloak','Leathers','']
-
-flavour_list = ['laughs a lot','patriotic: homeworld','piercing eyes of unusual colour', 'double down on one physical trait','double down one personality trait','double down one clothing trait', 'cultural dress','hair dyed a strange colour', 'androgenous', 'blind','wheelchair', 'glowing', 'missing limb, eye or ear', 'urbot', 'religious','anarchist', 'Yaru Clone','secretly rich', 'Working for another faction (click button below)', "former slave", "former soldier", "trademark weapon", "proud of their race",'mask', 'Has an alien pet','graceful','highly educated', 'ostentatious','touchy touchy', 'family oriented']
-
-male_names_list = ['Tobias Jann', 'Fafnir Breton', 'Ferris Zechiel', 'Monty Centrich', 'Thane Bucanan','Ivan Ryant','Brom Carthen','Sterling Quintan','Jaeger Wasrous','Gareth Catlow', 'Ursa Simril','Darius Paulsen','Vidar Allen','Derra Vale','Khan Inas','Jame Bibble','Shin Braze']
-
-female_names_list = ['Eveyln Wynter','Winona Thoran','Felicia Berrett','Freya Mattix','Trinity Sarrat','Lexi Lockley','Derra Vale','Grona Tillo','Nyree Alorr','Jane Dangir', 'Zena Cardinal', 'Miley Nafan','Tala Kale','Lora Halcyon','Jameela Brumen','Mira Das','Ayla Tane','Anillia Brando']
-
-aliases_list = ['Ace', 'Agony', 'Apex', 'Athena', 'Badger', 'Bingo', 'Black', 'Bolt', 'Brakes','Shatter', 'Carrot', 'Cash', 'Cosmo', 'Dash', 'Devil', 'Dipper', 'Echo','Eight', 'Elbows', 'Falcon', 'Fireball', 'Flex', 'Game', 'Gargoyle', 'Gear', 'Gonzo', 'Guns', 'Hammer', 'Headhunter', 'Helo', 'Hex', 'Highball', 'Hyper', 'Intake', 'Iris', 'Iron', 'Juggler', 'Juice', 'Junior', 'Karma', 'Lasher', 'Legend', 'Link', 'Loco', 'Mooch', 'Nails', 'Nemesis', 'Nova', 'Owl', 'Phoenix', 'Quirk', 'Raider', 'Razor', 'Rash', 'Skulls', 'Snaps', 'Snitch', 'Stinger', 'Syndrome', 'Tank', 'Tax', 'Titan', 'Tread', 'Under', 'Vandal', 'Vapor', 'Wraith', 'X-Ray', 'Yellow', 'Zen', 'Zenith', 'Zipper']
-
-race_list = ['white','black','east asian','indo asian', 'black-white mixed']
-
-factions_list = ['Guild of Engineers','Church of Stellar Flame','Counters Guild','Starless Veil','51st Legion','House Malklaith','Isotropa Max Secure', 'Starsmiths Guild', 'Cult of the Seekers',' Hegemonic News Network', 'Yaru (Makers Guild)', 'Concordiat Knights','Lost Legion', 'Scarlet Wolves', 'Vorex', 'Ashen Knives', 'Borniko Syndicate', 'Draxler’s Raiders', 'The Maelstrom', 'Echo Wave Riders', 'Janus Syndicate', 'Turner Society', 'Cobalt Syndicate', 'Dyrinek Gang', 'Wreckers', 'Sah’iir', 'Suneaters',' The Agony', 'Ashtari Cult',' Vignerons', 'Ghosts', 'Mendicants', 'Nightspeakers', 'Acolytes of Brashkadesh', 'Conclave 01', 'Vigilance']
-
 # =========== Functions ====================================
+
+@st.cache
+def import_data(data_file_name):
+    with open(data_file_name, "r") as file:
+        data = file.readlines()
+        data = [word for line in data for word in line.split()]
+    return data
 
 def generate_NPC():
     
@@ -38,9 +25,9 @@ def generate_NPC():
     alias = rd.sample(aliases_list,1)[0]    
     physical_appearance = rd.sample(physical_traits_list,2)
     clothing = rd.sample(clothing_list,1)[0]
-    race = rd.sample(race_list,1)[0]
+    race = rd.sample(races_list,1)[0]
     personality = rd.sample(personality_traits_list,2)
-    flavour = rd.sample(flavour_list,1)[0]
+    flavour = rd.sample(flavours_list,1)[0]
     
     description  = f"""\n
     NAME OPTIONS: {names}.\n
@@ -63,9 +50,19 @@ def select_faction():
     return faction
 
 def generate_extra_flavour():
-    new_flavour = rd.sample(flavour_list,1)
+    new_flavour = rd.sample(flavours_list,1)
     return new_flavour
 
+# Import the data
+personality_traits_list = import_data('personalities.txt')
+physical_traits_list = import_data('physical_traits.txt')
+clothing_list = import_data('clothing.txt')
+flavours_list = import_data('flavours.txt')
+male_names_list = import_data('male_names.txt')
+female_names_list = import_data('female_names.txt')
+aliases_list = import_data('aliases.txt')
+races_list = import_data('races.txt')
+factions_list = import_data('factions.txt')
 # ========== APP =============================
 
 "# Scum and Villainy NPC creator"
